e291136a46dbf56180dd2205e56157a9dca64aeb
dam <dam@203P10>
2022-02-04 11:59:16 +0100

    First commit

---
+++ src/mainpackage/Clase2.java
@@ -0,0 +1,14 @@
+package mainpackage;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Clase2 {
+
+    public void helloWorld() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
+    }
+}
