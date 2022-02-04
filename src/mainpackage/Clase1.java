8f1263d9982c6b863b3ec6ff462717b3c96e15f8
dam <dam@203P10>
2022-02-04 11:52:02 +0100

    First commit

---
+++ src/mainpackage/Clase1.java
@@ -0,0 +1,13 @@
+package mainpackage;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Clase1 {
+
+    public static void main(String[] args) {
+        String nombre = "Bruno";
+        System.out.println("Me llamo " + nombre);
+    }
+}
